020a945155e7dde9fe82b0a07d3ce4285f0736ca
collinfanning <cfann2@brockport.edu>
2020-10-07 21:30:50 -0400

    Added a little more stuff - about us page, fixed some global navigation.

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
 <!-- Nav Bar Creation-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <a class="navbar-brand" href="#">Nanno's Foods</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="true" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon">
             <!-- Put something here -->
         </span>
