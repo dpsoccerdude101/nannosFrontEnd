0f9af7a745d67cf4e037a38ec62d8eca933529c3
Dave Duncan <ddunc2@brockport.edu>
2020-11-18 23:29:17 -0500

    Some Dave design changes.

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -4,20 +4,11 @@ import { useTitle, navigateTo } from "haunted-router";
  export function Jumbotron() {
   useTitle("Home");
   return html`<!-- Jumbotron -->
-    <div style="background: transparent !important" class="jumbotron">
-    <div class="logo">
-      <img id="stick-man" src="img/nannos-stick-man.svg" alt="Nanno's">
-      <p class="brush-hand">
-        Taste the Nanno-ness!
-      </p>
+    <div class="jumbotron">
+      <div class="jumbotron-container">
+        <div class="jumbotron-welcome">Welcome!</div>
+        <div class="nannos-stick-man"></div>
       </div>
-      <hr class="my-4" />
-      <p>Click the button below to learn more about our store</p>
-      <p class="lead">
-        <a is="router-link" class="btn btn-primary btn-lg" href="/" role="button"
-          >Learn more</a
-        >
-      </p>
     </div>
     <!-- End Jumbotron -->`;
 }
