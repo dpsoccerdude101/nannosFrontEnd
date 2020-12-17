46dd090c013c35066224fd8fe0b7b5d9ec55f7a3
Dave Duncan <ddunc2@brockport.edu>
2020-12-16 22:29:55 -0500

    Corrected modifyMember layout

--- components/modifyMember.js
+++ components/modifyMember.js
@@ -28,100 +28,118 @@ export function ModifyMember() {
           .catch((error) => alert(error));
       }}
     >
-      <div className="container"><br />
-        <label htmlFor="CustomerId">
-            <b>Customer Id</b>
-        </label>
-        <input
-          type="text"
-          name="CustomerId"
-          value="${JSON.parse(sessionStorage.cust).CustomerId}"
-          required
-          readonly
-        /><br />
-        <label htmlFor="Name">
-          <b>Customer Name</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Name"
-          pattern="[a-z A-Z]{1,20}"
-          maxlength="20"
-          name="Name"
-          required
-          value="${JSON.parse(sessionStorage.cust).Name}"
-        /><br />
-        <label htmlFor="Address">
-          <b>Address</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Address"
-          maxlength="30"
-          name="Address"
-          required
-          value="${JSON.parse(sessionStorage.cust).Address}"
-        /><br />
-        <label htmlFor="City">
-          <b>City</b>
-        </label>
-        <input
-          type="text"
-          placeholder="City"
-          pattern="[a-z A-Z]{1,20}"
-          maxlength="20"
-          name="City"
-          required
-          value="${JSON.parse(sessionStorage.cust).City}"
-        /><br />
-        <label htmlFor="State">
-          <b>State</b>
-        </label>
-        <select name="State" 
-        selected="${JSON.parse(sessionStorage.cust).State}"
-        required>
-          ${States()}
-        </select><br />
-        <label htmlFor="Zip">
-          <b>Zip</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Zip Code"
-          pattern="[0-9]{5}"
-          maxlength="5"
-          name="Zip"
-          required
-          value="${JSON.parse(sessionStorage.cust).Zip}"
-        /><br />
-        <label htmlFor="Phone">
-          <b>Phone</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Phone Number"
-          pattern="[0-9]{10}"
-          maxlength="10"
-          name="Phone"
-          required
-          value="${JSON.parse(sessionStorage.cust).Phone}"
-        /><br />
-        <label htmlFor="Email">
-          <b>Customer Email</b>
-        </label>
-        <input
-          type="text"
-          placeholder="Customer Email"
-          pattern="[a-zA-Z0-9@.]{1,20}"
-          maxlength="20"
-          name="Email"
-          required
-          value="${JSON.parse(sessionStorage.cust).Email}"
-        /><br />
-        
-        </label>
-        <button type="submit">Modify Member</button>
-      </div>
+      <div class="form-container" className="container"><br />
+        <div class="form-row">
+          <div class="label-and-input">
+            <label htmlFor="CustomerId">
+                Customer Id
+            </label>
+            <input
+              type="text"
+              name="CustomerId"
+              value="${JSON.parse(sessionStorage.cust).CustomerId}"
+              required
+              readonly/>
+          </div>
+          <div class="label-and-input">
+            <label htmlFor="Name">
+              Name
+            </label>
+            <input
+            type="text"
+            placeholder="Name"
+            pattern="[a-z A-Z]{1,20}"
+            maxlength="20"
+            name="Name"
+            required
+            value="${JSON.parse(sessionStorage.cust).Name}"/>
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="label-and-input">
+            <label htmlFor="Address">
+              Address
+            </label>
+            <input
+            type="text"
+            placeholder="Address"
+            maxlength="30"
+            name="Address"
+            required
+            value="${JSON.parse(sessionStorage.cust).Address}"/>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="label-and-input">
+            <label htmlFor="City">
+              City
+            </label>
+            <input
+            type="text"
+            placeholder="City"
+            pattern="[a-z A-Z]{1,20}"
+            maxlength="20"
+            name="City"
+            required
+            value="${JSON.parse(sessionStorage.cust).City}"/>
+          </div>
+          <div class="label-and-input">
+            <label htmlFor="State">
+              State
+            </label>
+            <select name="State" 
+              selected="${JSON.parse(sessionStorage.cust).State}"
+              required>
+                ${States()}
+           </select>
+          </div>
+          <div class="label-and-input">
+            <label htmlFor="Zip">
+              Zip
+            </label>
+            <input
+            type="text"
+            placeholder="Zip Code"
+            pattern="[0-9]{5}"
+            maxlength="5"
+            name="Zip"
+            required
+            value="${JSON.parse(sessionStorage.cust).Zip}"/>
+          </div>
+        </div>
+        <div class="form-row">
+          <div class="label-and-input">
+            <label htmlFor="Phone">
+              Phone
+            </label>
+            <input
+              type="text"
+              placeholder="Phone Number"
+              pattern="[0-9]{10}"
+              maxlength="10"
+              name="Phone"
+              required
+              value="${JSON.parse(sessionStorage.cust).Phone}"/>
+          </div>
+          <div class="label-and-input">
+            <label htmlFor="Email">
+              E-mail
+            </label>
+            <input
+              type="text"
+              placeholder="Customer Email"
+              pattern="[a-zA-Z0-9@.]{1,20}"
+              maxlength="20"
+              name="Email"
+              required
+              value="${JSON.parse(sessionStorage.cust).Email}"/>
+          </div>
+        </div>
+        <div class="form-row">       
+          <button type="submit">Modify Member</button>
+        </div>
+      </div> 
     </form>
   `;
 }
